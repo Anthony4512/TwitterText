0c13d31f814098f3770e4fd834317b1b68a76b66
Anthony <00261404@student.necc.edu>
2017-07-17 14:31:54 -0400

    first commit...  changed requirements and TwitterText

--- twitter_text/TwitterText.py
+++ twitter_text/TwitterText.py
@@ -1,7 +1,8 @@
 import tweepy
 import twilio
 
-class TwitterText:
+
+class TwitterText(tweepy):
 
     def __init__(self, twitter_consumer_key, twitter_consumer_secret,
                  twitter_access_token, twitter_access_secret):
